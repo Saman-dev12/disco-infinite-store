11f0d6b9dd3c5514a350ca35480a97a7728e1c61
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2025-03-09 18:21:13 +0530

    added auth

--- src/lib/options.ts
+++ src/lib/options.ts
@@ -1,8 +1,36 @@
-import NextAuth from "next-auth"
+import { db } from "@/db/db";
+import NextAuth, { DefaultSession, NextAuthOptions } from "next-auth"
 import Discord from "next-auth/providers/discord"
-import { signIn } from "next-auth/react"
 
-export const authOptions = {
+declare module "next-auth" {
+    interface Session {
+      user: {
+        id: string;
+        name: string;
+        email: string;
+        image: string;
+      } & DefaultSession["user"];
+    }
+  
+    interface User {
+      id: string;
+      name: string;
+      email: string;
+      image: string;
+      password?: string;
+    }
+  }
+  
+  declare module "next-auth/jwt" {
+    interface JWT {
+      id: string;
+      name: string;
+      email: string;
+      image: string;
+    }
+  }
+
+export const authOptions:NextAuthOptions = {
   providers: [
     Discord({
       clientId: process.env.DISCORD_CLIENT_ID as string,
@@ -15,7 +43,56 @@ export const authOptions = {
     })
     
   ],
-  
+  callbacks: {
+
+    async signIn({ user }) {
+        if (!user.email) return false;
+        try {
+            const existingUser = db.user.findFirst({
+                where:{
+                    email:user.email
+                }
+            })
+            if(!existingUser){
+                await db.user.create({
+                    data:{
+                        email:user.email,
+                        name:user.name,
+                        image:user.image,
+                    }
+                })
+            }
+            return true;
+
+        } catch (error) {
+            console.error("Sign-in error:", error);
+            return false;
+        }
+    },
+    async jwt({ token, user }) {
+        if (user) {
+          token.id = user.id;
+          token.name = user.name ?? '';
+          token.email = user.email ?? '';
+          token.image = user.image ?? '';
+        }
+        return token;
+      },
+      async session({ session, token }) {
+        if (token) {
+          session.user.id = token.id;
+          session.user.name = token.name;
+          session.user.email = token.email;
+          session.user.image = token.image;
+        }
+        return session;
+      },
+    },
+  pages: {
+    signIn: "/auth/signin", 
+  },
+  secret: process.env.NEXTAUTH_SECRET as string,
+
 }
 
 export default NextAuth(authOptions)
