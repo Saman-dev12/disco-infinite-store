fe6cc948f39c4026e3b51e2da53104e111bbee7a
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2025-03-09 18:37:43 +0530

    Auth done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export default function Home() {
+  const session = useSession();
   return (
-    <>  
-    
-    </>
+    <div className="flex">  
+    {session.data?.user.email}
+    <button onClick={() => signIn("discord")}>Login</button>
+    <button onClick={() => signOut()}>Logout</button>
+    </div>
   );
 }
 
