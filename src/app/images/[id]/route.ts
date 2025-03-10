7470d4e323c434615ee0f8ac83c071fc7f95819a
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2025-03-11 01:04:13 +0530

    Error resolved

--- src/app/images/[id]/route.ts
+++ src/app/images/[id]/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/db/db"
 import { type NextRequest, NextResponse } from "next/server"
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   try {
     const { id } = await params
 
